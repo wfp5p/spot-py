43dcf49944fe4e00722056d5d2d317036dfc79be
Bill Pemberton <wfp5p@worldbroken.com>
2022-01-10 11:52:58 -0500

    pl_csv.py: now writes a format that csv_loader.pl understands
    The header isn't expected by csv_loader.
    Still needs to convert duration from ms to MM:SS

--- pl_csv.py
+++ pl_csv.py
@@ -8,36 +8,48 @@ requires SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET to be in shell env
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
+import csv
+import os
 
-def show_tracks():
+filepath = "/tmp/spot.csv"
+if os.path.exists(filepath):
+    print("File already exists!")
+    quit()
 
-    for i, item in enumerate(tracks['items']):
+# globals?  Damn Python seems to love them
+def write_csv():
 
+    # format that spot_csv.pl understands
+    csv_headers = ["performer", "title", "album", "duration_ms"]
+    tracklist = []
+    tracklist.append(csv_headers)
+
+
+    for item in tracks['items']:
         track = item['track']
 
         # Secondary query for album details
         album=sp.album( track['album']['uri'] )
 
-        output=["{:s}".format(track['artists'][0]['name']),
-                "{:s}".format(album['name']),
-                "{:s}".format(track['name']),
-                "{:02d}".format(track['track_number']),
-                "{:02d}".format(track['disc_number']),
-                "{:d}".format(track['duration_ms']),
-                "{:s}".format(album['release_date']),
-                "{:s}".format(album['release_date_precision']),
-                ]
+        track_info =[ track['artists'][0]['name'],
+                      track['name'],
+                      album['name'],
+                      track['duration_ms'],
+                    ]
+        tracklist.append(track_info)
 
-        # Form and print the ; separated string
-        print(";".join(output))
+    with open(filepath, 'w', encoding='utf-8') as file:
+        writer = csv.writer(file, dialect='unix', delimiter='|')
+        writer.writerows(tracklist)
 
 scope = "playlist-read-private"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 fields="tracks,next"
-results = sp.user_playlist('joewahoo', '6CoGeD2spqwCj5qneYEAt0', fields=fields)
+results = sp.user_playlist('joewahoo', '6CoGeD2spqwCj5qneYEAt0')
 tracks = results['tracks']
-show_tracks()
-while tracks['next']:
-    tracks = sp.next[tracks]
-    show_tracks()
+write_csv()
+# while tracks['next']:
+#     tracks = sp.next[tracks]
+#     show_tracks()
